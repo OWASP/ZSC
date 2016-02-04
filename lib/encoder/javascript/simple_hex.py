e3cfc810e47684b281e051cef83485e1b18d70a7
Ali Razmjoo <ali.razmjoo@owasp.org>
2016-02-04 23:16:07 +0330

    update javascript encoder simple_hex ( optimized + random varible and function name )
    random names will be useful if users want encode a file for more than
    one time!

--- lib/encoder/javascript/simple_hex.py
+++ lib/encoder/javascript/simple_hex.py
@@ -7,39 +7,49 @@ http://api.z3r0d4y.com/
 https://lists.owasp.org/mailman/listinfo/owasp-zsc-tool-project [ owasp-zsc-tool-project[at]lists[dot]owasp[dot]org ]
 '''
 import binascii
+import random
+import string
 from core.compatible import version
 _version = version()
 def encode(f):
-	arr = ''
+	hex_arr = []
+	val_names = []
 	data = ''
 	eval = ''
-	val = 'val'
 	n = 0
-	m = 1
+	m = 0
 	for line in f:
 		if _version is 2:
-			arr += str(binascii.b2a_hex(line)) + str('_')
+			hex_arr.append(str(binascii.b2a_hex(line)))
 		if _version is 3:
-			arr += (binascii.b2a_hex(str(line).encode('latin-1'))).decode('latin-1')
-	arr = arr.rsplit('_')[:-1]
-	for hex in arr:
+			hex_arr.append(str((binascii.b2a_hex(str(line).encode('latin-1'))).decode('latin-1')))
+	length = len(hex_arr)
+	while(length != 0):
+		val_names.append(''.join(random.choice(string.ascii_lowercase+string.ascii_uppercase) for i in range(50)))
+		length -= 1
+	for hex in hex_arr:
+		data += val_names[n] + ' = "' + str(hex) + '";\n'
 		n+=1
-		data += str(val) + str(n) + ' = "' + str(hex) + '";\n'
-	while(m<=n):
-		eval += str(val) + str(m) + '+'
+	while(m<=n-1):
+		eval += val_names[m] + '+'
 		m+=1
+	var_hex = ''.join(random.choice(string.ascii_lowercase+string.ascii_uppercase) for i in range(50))
+	var_str = ''.join(random.choice(string.ascii_lowercase+string.ascii_uppercase) for i in range(50))
+	var_counter = ''.join(random.choice(string.ascii_lowercase+string.ascii_uppercase) for i in range(50))
+	var_data = ''.join(random.choice(string.ascii_lowercase+string.ascii_uppercase) for i in range(50))
+	func_name = ''.join(random.choice(string.ascii_lowercase+string.ascii_uppercase) for i in range(50))
+	func_argv = ''.join(random.choice(string.ascii_lowercase+string.ascii_uppercase) for i in range(50))
 	f = '''
 %s
-
-function hex2str(hexx) {
-    var hex = hexx.toString();
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
+function %s(%s) {
+    var %s = %s.toString();
+    var %s = '';
+    for (var %s = 0; %s < %s.length; %s += 2)
+        %s += String.fromCharCode(parseInt(%s.substr(%s, 2), 16));
+    return %s;
 }
-data = %s;
-eval(hex2str(data));'''%(data,eval[:-1])
+%s = %s;
+eval(%s(%s));'''%(data,func_name,func_argv,var_hex,func_argv,var_str,var_counter,var_counter,var_hex,var_counter,var_str,var_hex,var_counter,var_str,var_data,eval[:-1],func_name,var_data)
 	return f
 
 def start(content):
