8ed1f9b612cb10524c660302ca273c30ab6e0ac4
Ali Razmjoo <ali.razmjoo@owasp.org>
2016-02-06 03:46:59 +0330

    fix readline , python 3.x , windows , issue #14

--- module/readline_windows/__init__.py
+++ module/readline_windows/__init__.py
@@ -6,18 +6,5 @@ https://github.com/Ali-Razmjoo/OWASP-ZSC
 http://api.z3r0d4y.com/
 https://lists.owasp.org/mailman/listinfo/owasp-zsc-tool-project [ owasp-zsc-tool-project[at]lists[dot]owasp[dot]org ]
 '''
-"""
-py-getch https://github.com/joeyespo/py-getch
---------
 
-Portable getch() for Python.
-
-:copyright: (c) 2013-2015 by Joe Esposito.
-:license: MIT, see LICENSE for more details.
-"""
-
-__version__ = '1.0.1'
-
-from .getch import getch
-
-__all__ = ['__version__', 'getch']
+pass
