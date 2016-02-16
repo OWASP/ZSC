e160194bae8499c07b9408f1934530b87f1d03bd
Ali Razmjoo <ali.razmjoo@owasp.org>
2016-02-16 14:07:55 +0330

    update mailing list

--- lib/generator/linux_x86/download.py
+++ lib/generator/linux_x86/download.py
@@ -4,7 +4,8 @@ OWASP ZSC | ZCR Shellcoder
 https://www.owasp.org/index.php/OWASP_ZSC_Tool_Project
 https://github.com/Ali-Razmjoo/OWASP-ZSC
 http://api.z3r0d4y.com/
-https://lists.owasp.org/mailman/listinfo/owasp-zsc-tool-project [ owasp-zsc-tool-project[at]lists[dot]owasp[dot]org ]
+https://groups.google.com/d/forum/owasp-zsc [ owasp-zsc[at]googlegroups[dot]com ]
+
 shellcode template used : http://shell-storm.org/shellcode/files/shellcode-57.php
 '''
 from core import stack
