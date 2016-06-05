d85db3aa061d5534fc6b6d4bbef1d6a5d433cc6e
Pratik151 <pratikpatel15133@gmail.com>
2016-06-05 16:17:04 +0530

    Added OWASP ZSC description

--- lib/generator/windows/exec.py
+++ lib/generator/windows/exec.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+'''
+OWASP ZSC | ZCR Shellcoder
+https://www.owasp.org/index.php/OWASP_ZSC_Tool_Project
+https://github.com/Ali-Razmjoo/OWASP-ZSC
+http://api.z3r0d4y.com/
+https://groups.google.com/d/forum/owasp-zsc [ owasp-zsc[at]googlegroups[dot]com ]
+'''
 from core import stack
 from math import ceil
 def exc(file_to_exec,file_name):
