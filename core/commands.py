631f60c2856ddbe7d85c5a4738f352c274b44e30
Pratik151 <pratikpatel15133@gmail.com>
2016-07-07 17:38:29 +0530

    Added php obfuscation

--- core/commands.py
+++ core/commands.py
@@ -62,7 +62,7 @@ commands = { #commands section
 				'python':
 						['simple_hex', 'simple_hex_rev', 'simple_base64_rev'],
 				'php':
-						['simple_hex', 'base64','simple_hex_rev'],
+						['simple_hex', 'base64','simple_hex_rev','base64_rev'],
 				'perl': 
 						['simple_hex', 'base64', 'simple_hex_rev','simple_base64_rev'],
                                 'ruby':
