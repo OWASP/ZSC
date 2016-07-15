25d681288503ce762fca037f80d4c63d6a08f217
Ali Razmjoo <ali.razmjoo@owasp.org>
2016-07-15 23:01:13 +0430

    update files header

--- lib/generator/linux_x86/chmod.py
+++ lib/generator/linux_x86/chmod.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 '''
-OWASP ZSC | ZCR Shellcoder
+OWASP ZSC
 https://www.owasp.org/index.php/OWASP_ZSC_Tool_Project
-https://github.com/Ali-Razmjoo/OWASP-ZSC
+https://github.com/zscproject/OWASP-ZSC
 http://api.z3r0d4y.com/
 https://groups.google.com/d/forum/owasp-zsc [ owasp-zsc[at]googlegroups[dot]com ]
 '''
