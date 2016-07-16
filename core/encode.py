b98db8c2e92040e017a21afde91000e87ef1a491
Pratik151 <pratikpatel15133@gmail.com>
2016-07-16 18:06:46 +0530

    fixed name

--- core/encode.py
+++ core/encode.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-OWASP ZSC | ZCR Shellcoder
+OWASP ZSC
 https://www.owasp.org/index.php/OWASP_ZSC_Tool_Project
 https://github.com/zscproject/OWASP-ZSC
 http://api.z3r0d4y.com/
