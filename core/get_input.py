1c8ab697b24b78cfbd1896e38afba4e626439d40
Ali Razmjoo <ali.razmjoo@owasp.org>
2016-07-17 06:53:32 +0430

    fix #66

--- core/get_input.py
+++ core/get_input.py
@@ -12,89 +12,112 @@ import binascii
 
 
 def _input(name, type, _while):
-    data = None
-    if _while is True:
-        if type == 'any':
-            while _while:
-                try:
-                    if version() is 3:
-                        data = input('%s%s>%s ' % (color.color('blue'), name,
-                                                   color.color('yellow')))
-                    if version() is 2:
-                        data = raw_input('%s%s>%s ' % (
-                            color.color('blue'), name, color.color('yellow')))
-                    break
-                except:
-                    write('wrong input!\n')
-                    pass
-        if type == 'hex':
-            while _while:
-                try:
-                    if version() is 3:
-                        data = input('%s%s>%s ' % (color.color('blue'), name,
-                                                   color.color('yellow')))
-                        binascii.b2a_hex(data[::-1].encode('latin-1')).decode(
-                            'latin-1')
-                    if version() is 2:
-                        data = raw_input('%s%s>%s ' % (
-                            color.color('blue'), name, color.color('yellow')))
-                        binascii.a2b_hex(data)
-                    break
-                except:
-                    warn('you must enter a hex value\n')
-                    pass
-        if type == 'int':
-            while _while:
-                try:
-                    if version() is 3:
-                        data = input('%s%s>%s ' % (color.color('blue'), name,
-                                                   color.color('yellow')))
-                        int(data)
-                    if version() is 2:
-                        data = raw_input('%s%s>%s ' % (
-                            color.color('blue'), name, color.color('yellow')))
-                        int(data)
-                    break
-                except:
-                    warn('you must enter a int value\n')
-                    pass
-    elif _while is False:
-        if type == 'any':
-            try:
-                if version() is 3:
-                    data = input('%s%s>%s ' % (color.color('blue'), name,
-                                               color.color('yellow')))
-                if version() is 2:
-                    data = raw_input('%s%s>%s ' % (color.color('blue'), name,
-                                                   color.color('yellow')))
-            except:
-                write('wrong input!\n')
-                pass
-        if type == 'hex':
-            try:
-                if version() is 3:
-                    data = input('%s%s>%s ' % (color.color('blue'), name,
-                                               color.color('yellow')))
-                    binascii.b2a_hex(data[::-1].encode('latin-1')).decode(
-                        'latin-1')
-                if version() is 2:
-                    data = raw_input('%s%s>%s ' % (color.color('blue'), name,
-                                                   color.color('yellow')))
-                    binascii.a2b_hex(data)
-            except:
-                warn('you must enter a hex value\n')
-                pass
-        if type == 'int':
-            try:
-                if version() is 3:
-                    data = input('%s%s>%s ' % (color.color('blue'), name,
-                                               color.color('yellow')))
-                    int(data)
-                if version() is 2:
-                    data = raw_input('%s%s>%s ' % (color.color('blue'), name,
-                                                   color.color('yellow')))
-                    int(data)
-            except:
-                warn('you must enter a int value\n')
-                pass
-    return data
+	data = None
+	if _while is True:
+		if type == 'any':
+			while _while:
+				try:
+					if version() is 3:
+						data = input('%s%s>%s ' % (color.color('blue'), name,
+												   color.color('yellow')))
+					if version() is 2:
+						data = raw_input('%s%s>%s ' % (
+							color.color('blue'), name, color.color('yellow')))
+					if data == '':
+						warn('input can\'t be empty! ')
+						_lets_error
+					break
+				except:
+					if data == None:
+						warn('interrupted by user!\nExit\n')
+						sys.exit(0)
+					write('wrong input!\n')
+					pass
+		if type == 'hex':
+			while _while:
+				try:
+					if version() is 3:
+						data = input('%s%s>%s ' % (color.color('blue'), name,
+												   color.color('yellow')))
+						binascii.b2a_hex(data[::-1].encode('latin-1')).decode(
+							'latin-1')
+					if version() is 2:
+						data = raw_input('%s%s>%s ' % (
+							color.color('blue'), name, color.color('yellow')))
+						binascii.a2b_hex(data)
+					if data == '':
+						warn('input can\'t be empty! ')
+						_lets_error
+					break
+				except:
+					if data == None:
+						warn('interrupted by user!\nExit\n')
+						sys.exit(0)
+					warn('you must enter a hex value\n')
+					pass
+		if type == 'int':
+			while _while:
+				try:
+					if version() is 3:
+						data = input('%s%s>%s ' % (color.color('blue'), name,
+												   color.color('yellow')))
+					if version() is 2:
+						data = raw_input('%s%s>%s ' % (
+							color.color('blue'), name, color.color('yellow')))
+					int(data) #if empty, jump to exception and error!
+					break
+				except:
+					if data == None:
+						warn('interrupted by user!\nExit\n')
+						sys.exit(0)
+					warn('you must enter a int value\n')
+					pass
+	elif _while is False:
+		if type == 'any':
+			try:
+				if version() is 3:
+					data = input('%s%s>%s ' % (color.color('blue'), name,
+											   color.color('yellow')))
+				if version() is 2:
+					data = raw_input('%s%s>%s ' % (color.color('blue'), name,
+												   color.color('yellow')))
+			except:
+				if data == None:
+					warn('interrupted by user!\nExit\n')
+					sys.exit(0)
+				write('wrong input!\n')
+				pass
+		if type == 'hex':
+			try:
+				if version() is 3:
+					data = input('%s%s>%s ' % (color.color('blue'), name,
+											   color.color('yellow')))
+					binascii.b2a_hex(data[::-1].encode('latin-1')).decode(
+						'latin-1')
+				if version() is 2:
+					data = raw_input('%s%s>%s ' % (color.color('blue'), name,
+												   color.color('yellow')))
+					binascii.a2b_hex(data)
+			except:
+				if data == None:
+					warn('interrupted by user!\nExit\n')
+					sys.exit(0)
+				warn('you must enter a hex value\n')
+				pass
+		if type == 'int':
+			try:
+				if version() is 3:
+					data = input('%s%s>%s ' % (color.color('blue'), name,
+											   color.color('yellow')))
+					int(data)
+				if version() is 2:
+					data = raw_input('%s%s>%s ' % (color.color('blue'), name,
+												   color.color('yellow')))
+					int(data)
+			except:
+				if data == None:
+					warn('interrupted by user!\nExit\n')
+					sys.exit(0)
+				warn('you must enter a int value\n')
+				pass
+	return data
