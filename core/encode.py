9d90be48f12fc7c594c8f4ef808c77e2c5836a8a
Nikhil. R <rnikhil96@outlook.com>
2017-07-17 18:42:45 +0530

    fix errors in encoding calls

--- core/encode.py
+++ core/encode.py
@@ -80,31 +80,31 @@ def encode_process(encode, shellcode, os, func):
         if encode == 'add_random':
             from lib.encoder.osx_x86.add_random import start
             return start(shellcode, func)
-        elif encode =='add_yourvalue':
+        elif 'add_' in encode:
             from lib.encoder.osx_x86.add_yourvalue import start
             return start(encode, shellcode, func)
         elif encode == 'dec':
             from lib.encoder.osx_x86.dec import start
             return start(shellcode, func)
-        elif encode == 'dec_timesyouwant':
+        elif 'dec_' in encode:
             from lib.encoder.osx_x86.dec_timesyouwant import start
             return start(encode, shellcode, func)
         elif encode == 'inc':
             from lib.encoder.osx_x86.inc import start
             return start(shellcode, func)
-        elif 'inc_timesyouwant' in encode:
+        elif 'inc_' in encode:
             from lib.encoder.osx_x86.inc_timesyouwant import start
             return start(encode, shellcode, func)
         elif encode == 'sub_random':
             from lib.encoder.osx_x86.sub_random import start
             return start(shellcode, func)
-        elif 'sub_yourvalue' in encode:
+        elif 'sub_' in encode:
             from lib.encoder.osx_x86.sub_yourvalue import start
             return start(encode, shellcode, func)
         elif encode == 'xor_random':
             from lib.encoder.osx_x86.xor_random import start
             return start(shellcode, func)
-        elif 'xor_yourvalue' in encode:
+        elif 'xor_' in encode:
             from lib.encoder.osx_x86.xor_yourvalue import start
             return start(encode, shellcode, func)
     return shellcode
